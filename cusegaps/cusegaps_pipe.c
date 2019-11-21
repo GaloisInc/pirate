dc0f59db0d98773644734a817fdedbf2f80cdb91
Michael Spiegel <michael.spiegel@twosixlabs.com>
2019-11-21 12:36:57 -0500

    Add copyright statement header to source files

--- cusegaps/cusegaps_pipe.c
+++ cusegaps/cusegaps_pipe.c
@@ -1,3 +1,17 @@
+/*
+ * This work was authored by Two Six Labs, LLC and is sponsored by a subcontract
+ * agreement with Galois, Inc.  This material is based upon work supported by
+ * the Defense Advanced Research Projects Agency (DARPA) under Contract No.
+ * HR0011-19-C-0103.
+ *
+ * The Government has unlimited rights to use, modify, reproduce, release,
+ * perform, display, or disclose computer software or computer software
+ * documentation marked with this legend. Any reproduction of technical data,
+ * computer software, or portions thereof marked with this legend must also
+ * reproduce this marking.
+ *
+ * Copyright 2019 Two Six Labs, LLC.  All rights reserved.
+ */
 #define FUSE_USE_VERSION 31
 #define _POSIX_C_SOURCE 200809L
 #define _GNU_SOURCE
--- libpirate/test/shmem_test.h
+++ libpirate/test/shmem_test.h
@@ -1,3 +1,17 @@
+/*
+ * This work was authored by Two Six Labs, LLC and is sponsored by a subcontract
+ * agreement with Galois, Inc.  This material is based upon work supported by
+ * the Defense Advanced Research Projects Agency (DARPA) under Contract No.
+ * HR0011-19-C-0103.
+ *
+ * The Government has unlimited rights to use, modify, reproduce, release,
+ * perform, display, or disclose computer software or computer software
+ * documentation marked with this legend. Any reproduction of technical data,
+ * computer software, or portions thereof marked with this legend must also
+ * reproduce this marking.
+ *
+ * Copyright 2019 Two Six Labs, LLC.  All rights reserved.
+ */
 #ifndef __PRIMITIVES_TEST_SHMEM_H
 #define __PRIMITIVES_TEST_SHMEM_H
 
