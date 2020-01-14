513c8436e2716c558b1156dc2fccada21da78dd2
Vitaly Tkachuk <vitaly.tkachuk@invincea.com>
2020-01-13 22:46:36 -0500

    Added copyright headers to the time demo source

--- time_demo/src/ts_crypto.c
+++ time_demo/src/ts_crypto.c
@@ -1,3 +1,18 @@
+/*
+ * This work was authored by Two Six Labs, LLC and is sponsored by a subcontract
+ * agreement with Galois, Inc.  This material is based upon work supported by
+ * the Defense Advanced Research Projects Agency (DARPA) under Contract No.
+ * HR0011-19-C-0103.
+ *
+ * The Government has unlimited rights to use, modify, reproduce, release,
+ * perform, display, or disclose computer software or computer software
+ * documentation marked with this legend. Any reproduction of technical data,
+ * computer software, or portions thereof marked with this legend must also
+ * reproduce this marking.
+ *
+ * Copyright 2019 Two Six Labs, LLC.  All rights reserved.
+ */
+
 #include <stdio.h>
 #include <string.h>
 #include <openssl/asn1.h>
