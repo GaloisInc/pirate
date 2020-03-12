00ec343a5ef3a8c2d4e727101350624d73066833
Michael Spiegel <michael.spiegel@twosixlabs.com>
2020-03-12 07:41:47 -0400

    Remove PIRATE_UNIT_TEST_MERCURY_SESSIONS

--- libpirate/test/mercury_test.cpp
+++ libpirate/test/mercury_test.cpp
@@ -256,7 +256,7 @@ static MercuryTestParam MercuryParams [] =
     // CH,LVL,SRC,DST,MSG_CNT,[5 MSGS],        EXP SESSION_ID
     {  0, 1,  1,  0,  0,      {0, 0, 0, 0, 0}, 0x00000001},
     {  2, 2,  2,  0,  0,      {0, 0, 0, 0, 0}, 0x00000002},
-#ifdef PIRATE_UNIT_TEST_MERCURY_SESSIONS
+#if 0
     {  1, 1,  1,  2,  5,      {1, 3, 0, 0, 0}, 0xECA51756},
     {  3, 2,  2,  1,  5,      {1, 3, 0, 0, 0}, 0x67FF90F4},
     {  0, 1,  1,  2,  5,      {1, 6, 5, 0, 0}, 0x6BB83E13},
