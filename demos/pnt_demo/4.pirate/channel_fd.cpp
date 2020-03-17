cad7f9cb81cb20ec81844664fe8fcad80a87ddd1
Michael Spiegel <michael.spiegel@twosixlabs.com>
2020-03-17 14:06:11 -0400

    4.pirate: Use generic libpirate channels

--- demos/pnt_demo/4.pirate/channel_fd.cpp
+++ demos/pnt_demo/4.pirate/channel_fd.cpp
@@ -6,34 +6,35 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-void gdCheckedWrite(const std::string& path, int gd, const void* buf, size_t n) {
+void gdCheckedWrite(const std::string& config, int gd, const void* buf, size_t n) {
   ssize_t cnt = pirate_write(gd, buf, n);
   if (cnt == -1) {
-    channel_errlog([path](FILE* f) { fprintf(f, "%s write failed (error = %d).", path.c_str(), errno); });
+    channel_errlog([config](FILE* f) { fprintf(f, "%s write failed (error = %d).", config.c_str(), errno); });
     exit(-1);
   }
   if (cnt < n) {
-    channel_errlog([path, cnt](FILE* f) { fprintf(f, "%s incomplete write (bytes = %zd).", path.c_str(), cnt); });
+    channel_errlog([config, cnt](FILE* f) { fprintf(f, "%s incomplete write (bytes = %zd).", config.c_str(), cnt); });
     exit(-1);
   }
 }
 
-void piratePipe(const std::string& path, int gd) {
-  int rv;
+void piratePipe(const std::string& config, int gd) {
   pirate_channel_param_t param;
 
-  pirate_init_channel_param(PIPE, &param);
-  strncpy(param.channel.pipe.path, path.c_str(), sizeof(param.channel.pipe.path));
+  if (pirate_parse_channel_param(config.c_str(), &param) < 0) {
+    channel_errlog([config](FILE* f) { fprintf(f, "%s unable to parse channel parameter", config.c_str()); });
+    exit(-1);
+  }
   if (pirate_set_channel_param(gd, O_RDONLY, &param) < 0) {
-    channel_errlog([](FILE* f) { fprintf(f, "unable to set read channel parameter"); });
+    channel_errlog([config](FILE* f) { fprintf(f, "%s unable to set read channel parameter", config.c_str()); });
     exit(-1);
   }
   if (pirate_set_channel_param(gd, O_WRONLY, &param) < 0) {
-    channel_errlog([](FILE* f) { fprintf(f, "unable to set write channel parameter"); });
+    channel_errlog([config](FILE* f) { fprintf(f, "%s unable to set write channel parameter", config.c_str()); });
     exit(-1);
   }
   if (pirate_pipe(gd, O_RDWR) < 0) {
-    channel_errlog([](FILE* f) { fprintf(f, "unable to open pirate pipe"); });
+    channel_errlog([config](FILE* f) { fprintf(f, "%s unable to open pirate pipe", config.c_str()); });
     exit(-1);
   }
 }
--- demos/pnt_demo/4.pirate/channel_fd.h
+++ demos/pnt_demo/4.pirate/channel_fd.h
@@ -10,32 +10,34 @@
 #include <unistd.h>
 
 /** Write the bytes to a file descriptor, and check that all bytes were written. */
-void gdCheckedWrite(const std::string& path, int gd, const void* buf, size_t n);
-void piratePipe(const std::string& path, int gd);
+void gdCheckedWrite(const std::string& config, int gd, const void* buf, size_t n);
+void piratePipe(const std::string& config, int gd);
 
 template<typename T>
-Sender<T> gdSender(const std::string& path, int gd) {
-  auto sendFn = [path, gd](const T& d) { gdCheckedWrite(path, gd, &d, sizeof(T)); };
+Sender<T> gdSender(const std::string& config, int gd) {
+  auto sendFn = [config, gd](const T& d) { gdCheckedWrite(config, gd, &d, sizeof(T)); };
   auto closeFn = [gd]() { pirate_close(gd, O_WRONLY); };
   return Sender<T>(sendFn, closeFn);
 }
 
 template<typename T>
-Sender<T> pirateSender(const std::string& path, int gd) {
+Sender<T> pirateSender(const std::string& config, int gd) {
     pirate_channel_param_t param;
 
-    pirate_init_channel_param(PIPE, &param);
-    strncpy(param.channel.pipe.path, path.c_str(), sizeof(param.channel.pipe.path));
+    if (pirate_parse_channel_param(config.c_str(), &param) < 0) {
+        channel_errlog([config](FILE* f) { fprintf(f, "%s unable to set channel parameter", config.c_str()); });
+        exit(-1);
+    }
     if (pirate_set_channel_param(gd, O_WRONLY, &param) < 0) {
-        channel_errlog([](FILE* f) { fprintf(f, "Unable to set channel parameter"); });
+        channel_errlog([config](FILE* f) { fprintf(f, "%s unable to set channel parameter", config.c_str()); });
         exit(-1);
     }
     if (pirate_open(gd, O_WRONLY) < 0) {
-        channel_errlog([path](FILE* f) { fprintf(f, "Open %s failed (error = %d)", path.c_str(), errno); });
+        channel_errlog([config](FILE* f) { fprintf(f, "Open %s failed (error = %d)", config.c_str(), errno); });
         exit(-1);
     }
     // Return sender
-    return gdSender<T>(path, gd);
+    return gdSender<T>(config, gd);
 }
 
 /**
@@ -45,20 +47,20 @@ Sender<T> pirateSender(const std::string& path, int gd) {
  * where we expect each call will read a precise number of bytes.
  */
 template<typename T>
-void gdDatagramReadMessages(const std::string& path, int gd, std::function<void(const T&)> f)
+void gdDatagramReadMessages(const std::string& config, int gd, std::function<void(const T&)> f)
 {
   while (true) {
     T x;
     ssize_t cnt = pirate_read(gd, &x, sizeof(T));
     if (cnt == -1) {
-      channel_errlog([path](FILE* f) { fprintf(f, "Read %s failed (error = %d)", path.c_str(), errno); });
+      channel_errlog([config](FILE* f) { fprintf(f, "Read %s failed (error = %d)", config.c_str(), errno); });
       exit(-1);
     }
     if (cnt == 0) { 
       break;
     }
     if (cnt != sizeof(T)) {
-      channel_errlog([path, cnt](FILE* f) { fprintf(f, "Read %s incorrect bytes (expected = %lu, received = %lu)", path.c_str(), sizeof(T), cnt); });
+      channel_errlog([config, cnt](FILE* f) { fprintf(f, "Read %s incorrect bytes (expected = %lu, received = %lu)", config.c_str(), sizeof(T), cnt); });
       exit(-1);
     }
     f(x);
@@ -67,29 +69,28 @@ void gdDatagramReadMessages(const std::string& path, int gd, std::function<void(
 }
 
 template<typename T>
-Receiver<T> gdReceiver(const std::string& path, int gd) {
-    return [path, gd](std::function<void (const T& d)> fn) {
-      gdDatagramReadMessages<T>(path, gd, fn);
+Receiver<T> gdReceiver(const std::string& config, int gd) {
+    return [config, gd](std::function<void (const T& d)> fn) {
+      gdDatagramReadMessages<T>(config, gd, fn);
     };
 }
 
-/** 
- * Create a message receiver that creates a unix SEQPACKET socket on the given path.
- */
 template<typename T>
-Receiver<T> pirateReceiver(const std::string& path, int gd) {
+Receiver<T> pirateReceiver(const std::string& config, int gd) {
     pirate_channel_param_t param;
 
-    pirate_init_channel_param(PIPE, &param);
-    strncpy(param.channel.pipe.path, path.c_str(), sizeof(param.channel.pipe.path));
+    if (pirate_parse_channel_param(config.c_str(), &param) < 0) {
+        channel_errlog([config](FILE* f) { fprintf(f, "%s unable to set channel parameter", config.c_str()); });
+        exit(-1);
+    }
     if (pirate_set_channel_param(gd, O_RDONLY, &param) < 0) {
-        channel_errlog([](FILE* f) { fprintf(f, "Unable to set channel parameter"); });
+        channel_errlog([config](FILE* f) { fprintf(f, "%s unable to set channel parameter", config.c_str()); });
         exit(-1);
     }
     if (pirate_open(gd, O_RDONLY) < 0) {
-        channel_errlog([path](FILE* f) { fprintf(f, "Open %s failed (error = %d)", path.c_str(), errno); });
+        channel_errlog([config](FILE* f) { fprintf(f, "Open %s failed (error = %d)", config.c_str(), errno); });
         exit(-1);
     }
-
-    return gdReceiver<T>(path, gd);
+    // Return sender
+    return gdReceiver<T>(config, gd);
 }
