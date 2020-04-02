80666230fbd5825c082210e46f9182d260c05603
Vitaly Tkachuk <vitaly.tkachuk@invincea.com>
2020-04-02 01:19:59 -0400

    PNT demo version 4 uses new libpirate API for obtaining channel configuration

--- demos/pnt_demo/4.pirate/channel_fd.cpp
+++ demos/pnt_demo/4.pirate/channel_fd.cpp
@@ -6,14 +6,18 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-void gdCheckedWrite(const std::string& config, int gd, const void* buf, size_t n) {
+void gdCheckedWrite(int gd, const void* buf, size_t n) {
   ssize_t cnt = pirate_write(gd, buf, n);
   if (cnt == -1) {
-    channel_errlog([config](FILE* f) { fprintf(f, "%s write failed (error = %d).", config.c_str(), errno); });
+    char config[128];
+    pirate_get_channel_description(gd, config, sizeof(config));
+    channel_errlog([config](FILE* f) { fprintf(f, "%s write failed (error = %d).", config, errno); });
     exit(-1);
   }
   if (cnt < n) {
-    channel_errlog([config, cnt](FILE* f) { fprintf(f, "%s incomplete write (bytes = %zd).", config.c_str(), cnt); });
+    char config[128];
+    pirate_get_channel_description(gd, config, sizeof(config));
+    channel_errlog([config, cnt](FILE* f) { fprintf(f, "%s incomplete write (bytes = %zd).", config, cnt); });
     exit(-1);
   }
 }
