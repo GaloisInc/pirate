d01f5dda9582598d765b08f83b5fde897700aee0
Michael Spiegel <michael.spiegel@twosixlabs.com>
2020-04-08 15:46:52 -0400

    run_mercury.sh loopback driver not threadsafe

--- demos/pnt_demo/4.pirate/run_mercury.sh
+++ demos/pnt_demo/4.pirate/run_mercury.sh
@@ -6,7 +6,7 @@ GPS_TO_UAV=mercury,2,2,1,1,3,0,0,0
 # position
 UAV_TO_TGT=mercury,1,1,2,1,3,0,0,0
 # distance
-RF_TO_TGT=mercury,1,1,2,1,6,5,0,0
+RF_TO_TGT=udp_socket,127.0.0.1,4321
 
 PNT_ORANGE=pnt_orange
 PNT_GREEN=pnt_green
