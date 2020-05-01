420883fbcc566eba7660896d2d57b7afa08e1c95
Vitaly Tkachuk <vitaly.tkachuk@invincea.com>
2020-05-01 00:43:55 -0400

    Improved benchmark script: - Added command-line option parser - Added ability to split into reader/writer roles in order to run on separate machines - Added option for specifying message lengths - Added option for specifying number of iterations per message length

--- libpirate/bench/bench.py
+++ libpirate/bench/bench.py
@@ -1,50 +1,55 @@
 #!/usr/bin/env python3
 
 import re
-
-import subprocess
-from subprocess import STDOUT, PIPE
-
 import sys
+import subprocess
+import argparse
 
-MESSAGE_SIZES = []
-
-for i in range(0, 21):
-    MESSAGE_SIZES.append(2 ** i)
-
-def usage():
-    print("Usage: " + sys.argv[0] + " thr " + " scenario-name [test channel] [sync channel]")
-    print("Usage: " + sys.argv[0] + " lat " + " scenario-name [test channel 1] [test channel 2] [sync channel]")
-    exit(1)
-
-if len(sys.argv) < 5:
-    usage()
+def parse_sizes(arg):
+    args = arg.split(',')
+    params = [int(i.strip()) for i in args[1:]]
+    if args[0] == 'pow':
+        return [2 ** i for i in range(*params)]
+    elif args[0] == 'inc':
+        return range(*params)
+    print("Invalid length specifications '{}'".format(arg), file=sys.stderr)
+    sys,exit(1)
 
 def main():
-    suite = sys.argv[1]
-    scenario = sys.argv[2]
+    p = argparse.ArgumentParser(description="GAPS pirate benchmark suite")
+    p.add_argument("-t", "--test_type", help="Test suite type", choices=["thr", "lat"], default="thr")
+    p.add_argument("-r", "--role", help="Bench component", choices=["reader", "writer", "both"], default="both")
+    p.add_argument("-n", "--scenario_name", help="Test scenario type", default="Default bench")
+    p.add_argument("-c1", "--test_channel_1", help="Test channel 1 configuration", required=True)
+    p.add_argument("-c2", "--test_channel_2", help="Test channel 2 configuration")
+    p.add_argument("-s", "--sync_channel", help="Synchronization channel specification", default="tcp_socket,127.0.0.1,10000")
+    p.add_argument("-l", "--message_sizes", help="Test sizes <pow,start,stop|inc,start,stop,step>", type=parse_sizes)
+    p.add_argument("-i", "--iterations", help="Number of iterations for each test size", type=int)
+    args = p.parse_args()
 
-    if suite != "thr" and suite != "lat":
-        usage()
+    if args.message_sizes is None:
+        args.message_sizes = [2 ** i for i in range(21)]
 
-    if suite == "thr" and len(sys.argv) != 5:
-        usage()
+    if args.test_type == 'lat' and args.test_channel_2 is None:
+        print("Must provide channel 2 configuration for 'lat' test type", file=sys.stderr)
+        sys,exit(1)
 
-    if suite == "lat" and len(sys.argv) != 6:
-        usage()
-
-    if suite == "thr":
-        filename1 = "bench_thr_writer"
-        filename2 = "bench_thr_reader"
+    if args.test_type == "thr":
+        writer_app = "bench_thr_writer"
+        reader_app = "bench_thr_reader"
+        channel_args = [args.test_channel_1, args.sync_channel]
         pattern = re.compile(r'average throughput: ([0-9.]+) MB/s')
-        iters = 64
+        if args.iterations is None:
+            args.iterations = 64
     else:
-        filename1 = "bench_lat1"
-        filename2 = "bench_lat2"
+        writer_app = "bench_lat1"
+        reader_app = "bench_lat2"
+        channel_args = [args.test_channel_1, args.test_channel_2, args.sync_channel]
         pattern = re.compile(r'average latency: (\d+) ns')
-        iters = 32
+        if args.iterations is None:
+            args.iterations = 32
 
-    for message_size in MESSAGE_SIZES:
+    for message_size in args.message_sizes:
         if message_size < 32:
             nbytes = 1_000_000
         elif message_size < 1024:
@@ -53,17 +58,23 @@ def main():
             nbytes = 100_000_000
         else:
             nbytes = 1_000_000_000
-        args1 = ["./" + filename1] + sys.argv[3:] + [str(message_size), str(nbytes)]
-        args2 = ["./" + filename2] + sys.argv[3:] + [str(message_size), str(nbytes)]
-        for _ in range(iters):
-            print(scenario, message_size, end=" ")
-            sys.stdout.flush()
-            p1 = subprocess.Popen(args1)
-            p2 = subprocess.Popen(args2, stdout=PIPE)
-            p1.wait()
-            out, _ = p2.communicate()
-            results = pattern.search(out.decode('utf-8'))
-            print(results.group(1))
+
+        test_args = channel_args + [str(message_size), str(nbytes)]
+
+        for _ in range(args.iterations):
+            if args.role == "both" or args.role == "writer":
+                writer_proc = subprocess.Popen(["./" + writer_app] + test_args)
+
+            if args.role == "both" or args.role == "reader":
+                reader_proc = subprocess.Popen(["./" + reader_app] + test_args, stdout=subprocess.PIPE)
+
+            if args.role == "both" or args.role == "writer":
+                writer_proc.wait()
+
+            if args.role == "both" or args.role == "reader":
+                out, _ = reader_proc.communicate()
+                results = pattern.search(out.decode('utf-8'))
+                print(args.scenario_name, message_size, results.group(1), flush=True)
 
 if __name__ == "__main__":
     main()
