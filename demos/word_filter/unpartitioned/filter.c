c586bba648b2627e7cb77c2fe0964149e64a16e2
Joe Hendrix <jhendrix@galois.com>
2020-07-16 10:04:26 -0700

    Update word filter to bound message size.

--- demos/word_filter/unpartitioned/filter.c
+++ demos/word_filter/unpartitioned/filter.c
@@ -1,6 +1,3 @@
-#include "libpirate.h"
-
-#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -14,8 +11,8 @@ static const char *word_list[]
   "rightsizing", "synergies",
 };
 
+// This belongs in another enclave
 static void censor(char *msg)
-__attribute__((pirate_enclave("word_filter")))
 {
   for (size_t i = 0; i < ARRAY_LEN(word_list); i++) {
     char const* word = word_list[i];
@@ -30,21 +27,29 @@ int main(void) {
 
   char *line = NULL;
   size_t line_sz = 0;
+  const size_t maxLen = 80;
 
   for(;;) {
     printf("Input> ");
     fflush(stdout);
 
     ssize_t len = getline(&line, &line_sz, stdin);
+    
     if (len < 0) {
       puts("\n");
       break;
+    }
+
+    // Bound string size and maxLen
+    if (len > maxLen) {
+      line[maxLen] = 0; // This is valid because line_sz must be at least len +1
     }
 
     censor(line);
 
     printf("Response> %s", line);
   }
+  free(line);
 
   return 0;
 }
