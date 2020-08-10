a0fed7f6d3c49107820b2601b521b62d9ea7d45d
Michael Spiegel <michael.spiegel@twosixlabs.com>
2020-08-10 09:55:10 -0400

    libpirate windows cleanup pirate_write_mtu api

--- libpirate/windows/ge_eth.h
+++ libpirate/windows/ge_eth.h
@@ -34,7 +34,7 @@ int pirate_ge_eth_open(void* _param, void* _ctx);
 int pirate_ge_eth_close(void* _ctx);
 SSIZE_T pirate_ge_eth_read(const void* _param, void* _ctx, void* buf, size_t count);
 SSIZE_T pirate_ge_eth_write(const void* _param, void* _ctx, const void* buf, size_t count);
-SSIZE_T pirate_ge_eth_write_mtu(const void* _param, void *_ctx);
+SSIZE_T pirate_ge_eth_write_mtu(const void* _param);
 
 #define PIRATE_GE_ETH_CHANNEL_FUNCS { pirate_ge_eth_parse_param, pirate_ge_eth_get_channel_description, pirate_ge_eth_open, pirate_ge_eth_close, pirate_ge_eth_read, pirate_ge_eth_write, pirate_ge_eth_write_mtu }
 
