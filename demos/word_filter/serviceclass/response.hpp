414ac0e70d91b68da279a803ab8e168ff15bd981
Eric Mertens <emertens@galois.com>
2020-09-11 16:01:43 -0700

    initial exception handling in serviceclass

--- demos/word_filter/serviceclass/response.hpp
+++ demos/word_filter/serviceclass/response.hpp
@@ -5,7 +5,7 @@
 
 template <typename T>
 struct Response {
-    Response(uint64_t id, T t) : tx_id(id), content(t) {}
+    Response(uint64_t id, std::optional<T> t) : tx_id(id), content(t) {}
     uint64_t tx_id;
     std::optional<T> content;
 };
@@ -14,7 +14,7 @@ template<typename T>
 struct Serialize<Response<T>> {
     static constexpr size_t size = sizeof(uint64_t) + Serialize<T>::size;
     static Response<T> fromBuffer(char const* buffer) {
-        return Response<T>(deserialize<uint64_t>(buffer), deserialize<T>(buffer + sizeof (uint64_t)));
+        return Response<T>(deserialize<uint64_t>(buffer), deserialize<std::optional<T>>(buffer + sizeof (uint64_t)));
     }
     static void toBuffer(std::vector<char> & buffer, Response<T> const& res) {
         serialize(buffer, res.tx_id);
