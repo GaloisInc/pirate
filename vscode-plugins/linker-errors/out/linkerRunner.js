d1f2095b101ec353fd3e290c71fe4d8ed3b293b5
Karl Smeltzer <karl@galois.com>
2020-10-15 16:34:05 -0700

    Actually parse linker errors

--- vscode-plugins/linker-errors/out/linkerRunner.js
+++ vscode-plugins/linker-errors/out/linkerRunner.js
@@ -13,41 +13,70 @@ exports.LinkerRunner = void 0;
 const vscode = require("vscode");
 const child_process = require("child_process");
 class LinkerRunner {
+    // The public entry point for the linker, which just needs a way to register diagnostics.
     runLinker(diagnosticCollection) {
         return __awaiter(this, void 0, void 0, function* () {
             let diagnostics = this.linkProject();
             for (let file of diagnostics.keys()) {
                 const fileDiags = diagnostics.get(file);
                 if (fileDiags) {
-                    vscode.workspace.findFiles(file, null, 1).then((uriArray) => {
-                        diagnosticCollection.set(uriArray[0], fileDiags);
-                    });
+                    this.makeUnique(fileDiags);
+                    vscode.workspace.findFiles(file, null, 1).then((uriArray) => __awaiter(this, void 0, void 0, function* () {
+                        const newDiags = yield this.adjustRangeStart(uriArray[0], fileDiags);
+                        diagnosticCollection.set(uriArray[0], newDiags);
+                    }));
                 }
             }
         });
     }
-    // This is a bit of a hack, but currently the linker conflict lines are of the
-    // form "Symbol <name> needed, but has unmet requirement <capability>".  For
-    // now, it seems sufficient to just look at the first word.
-    isLinkerConflict(text) {
-        console.log("Checking: " + text);
-        return text.trimLeft().startsWith("Symbol");
+    // Eliminate duplicate diagnostics, which requires looking just at the error
+    // message and range since the whole object will generally compare as not being
+    // equal no matter what.
+    makeUnique(items) {
+        return items;
     }
+    // Determines if a line of output text is one we want to parse
+    isLinkerConflict(text) {
+        return text.trimLeft().startsWith("ld.lld");
+    }
+    // Parses linker errors of the following specific form (assumes the ld.lld:
+    // error bit has been trimmed already)
+    // 
+    // Symbol foo needed, but has unmet requirement high
+    // used at main.c:7:(.text.main+0x15)
+    //
+    // The two parameters are these two lines of text
     parseLinkerError(err, loc) {
-        const errWords = err.split(" ");
+        const errWords = err.trimLeft().split(" ");
         const symbolName = errWords[1];
         const capability = errWords[7];
-        const locParts = loc.split(":");
+        const locParts = loc.trimLeft().split(":");
         const fileName = locParts[0].split(" ")[2];
         const lineNumber = parseInt(locParts[1]);
-        let start = new vscode.Position(lineNumber, 0);
-        let end = new vscode.Position(lineNumber, 999);
-        let range = new vscode.Range(start, end);
+        // We don't know the real start character so we set it to zero and adjust it later.
+        let range = new vscode.Range(lineNumber - 1, 0, lineNumber - 1, 999);
         return [
             new vscode.Diagnostic(range, "Reference to symbol " + symbolName + " requires the function to have capability " + capability, vscode.DiagnosticSeverity.Error),
             fileName
         ];
     }
+    // For a diagnostic and the corresponding file Uri, we adjust the range of the
+    // diagnostic to skip over whitespace characters at the start of the line. This
+    // is a purely aesthetic and probably somewhat slow operation, but highlighting
+    // potential many whitespace characters looks quite bad.
+    adjustRangeStart(uri, diags) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const bytes = yield vscode.workspace.fs.readFile(uri);
+            const lines = bytes.toString().split("\n");
+            diags.forEach((diag, _idx, _arr) => {
+                const line = lines[diag.range.start.line];
+                diag.range = new vscode.Range(diag.range.start.line, line.search(/\S|$/), diag.range.end.line, 999);
+            });
+            return diags;
+        });
+    }
+    // The main entry point for calling out to clang, providing a mapping from
+    // filenames to arrays of errors
     linkProject() {
         let diags = new Map();
         const folders = vscode.workspace.workspaceFolders;
@@ -55,30 +84,36 @@ class LinkerRunner {
             return diags;
         }
         const options = {
-            cwd: folders[0].uri.fsPath
+            cwd: folders[0].uri.fsPath,
+            encoding: 'utf8',
+            shell: true
         };
-        child_process.exec("/home/karl/pirate/pirate-llvm/build/bin/clang -fuse-ld=lld -fdiagnostics-print-source-range-info -fdiagnostics-parseable-fixits -ffunction-sections -fdata-sections -Wl,-enclave,low *.c", options, (error, stdout, stderr) => {
-            const lines = stderr.split("\n");
-            let currLine = 0;
-            while (currLine < lines.length) {
-                let parts = lines[currLine].split(":");
-                if (this.isLinkerConflict(parts[2])) {
-                    const [diag, fileName] = this.parseLinkerError(parts[2], lines[++currLine]);
-                    let currentDiags = diags.get(fileName);
-                    if (currentDiags) {
-                        currentDiags.push(diag);
-                        diags.set(fileName, currentDiags);
-                    }
-                    else {
-                        diags.set(fileName, [diag]);
-                    }
+        let procResult = child_process.spawnSync("/home/karl/galois/pirate/pirate-llvm/build/bin/clang", ["-g",
+            "-fuse-ld=lld",
+            "-ffunction-sections",
+            "-fdata-sections",
+            "-Wl,-enclave,low",
+            "*.c"
+        ], options);
+        const errText = procResult.stderr.toString('utf8');
+        const lines = errText.split("\n");
+        let currLine = 0;
+        while (currLine < lines.length - 1) {
+            if (this.isLinkerConflict(lines[currLine])) {
+                const parts = lines[currLine].split(":");
+                const [diag, fileName] = this.parseLinkerError(parts[2], lines[currLine + 1]);
+                currLine++;
+                const currentDiags = diags.get(fileName);
+                if (currentDiags) {
+                    currentDiags.push(diag);
+                    diags.set(fileName, currentDiags);
                 }
                 else {
-                    console.log("ignored line: " + lines[currLine]);
+                    diags.set(fileName, [diag]);
                 }
-                currLine++;
             }
-        });
+            currLine++;
+        }
         return diags;
     }
 }
--- vscode-plugins/linker-errors/src/linkerRunner.ts
+++ vscode-plugins/linker-errors/src/linkerRunner.ts
@@ -2,38 +2,51 @@ import * as vscode from "vscode";
 import * as child_process from "child_process";
 
 export class LinkerRunner {
+  // The public entry point for the linker, which just needs a way to register diagnostics.
   public async runLinker(diagnosticCollection: vscode.DiagnosticCollection) {
     let diagnostics = this.linkProject();
     for (let file of diagnostics.keys()) {
       const fileDiags = diagnostics.get(file);
       if (fileDiags) {
-        vscode.workspace.findFiles(file, null, 1).then((uriArray) => {
-          diagnosticCollection.set(uriArray[0], fileDiags);
+        this.makeUnique(fileDiags);
+        vscode.workspace.findFiles(file, null, 1).then(async (uriArray) => {
+          const newDiags = await this.adjustRangeStart(uriArray[0], fileDiags);
+          diagnosticCollection.set(uriArray[0], newDiags);
         });
       }
     }
   }
 
-  // This is a bit of a hack, but currently the linker conflict lines are of the
-  // form "Symbol <name> needed, but has unmet requirement <capability>".  For
-  // now, it seems sufficient to just look at the first word.
-  private isLinkerConflict(text: string): boolean {
-    console.log("Checking: " + text);
-    return text.trimLeft().startsWith("Symbol");
+  // Eliminate duplicate diagnostics, which requires looking just at the error
+  // message and range since the whole object will generally compare as not being
+  // equal no matter what.
+  private makeUnique(items: Array<vscode.Diagnostic>): Array<vscode.Diagnostic> {
+    return items;
   }
 
+  // Determines if a line of output text is one we want to parse
+  private isLinkerConflict(text: string): boolean {
+    return text.trimLeft().startsWith("ld.lld");
+  }
+
+  // Parses linker errors of the following specific form (assumes the ld.lld:
+  // error bit has been trimmed already)
+  // 
+  // Symbol foo needed, but has unmet requirement high
+  // used at main.c:7:(.text.main+0x15)
+  //
+  // The two parameters are these two lines of text
   private parseLinkerError(err: string, loc: string): [vscode.Diagnostic, string] {
-    const errWords = err.split(" ");
+    const errWords = err.trimLeft().split(" ");
     const symbolName = errWords[1];
     const capability = errWords[7];
 
-    const locParts = loc.split(":");
+    const locParts = loc.trimLeft().split(":");
     const fileName = locParts[0].split(" ")[2];
     const lineNumber = parseInt(locParts[1]);
 
-    let start: vscode.Position = new vscode.Position(lineNumber, 0);
-    let end: vscode.Position = new vscode.Position(lineNumber, 999);
-    let range: vscode.Range = new vscode.Range(start, end);
+    // We don't know the real start character so we set it to zero and adjust it later.
+    let range: vscode.Range = new vscode.Range(lineNumber - 1, 0, lineNumber - 1, 999);
     return [
       new vscode.Diagnostic(
         range,
@@ -42,38 +55,62 @@ export class LinkerRunner {
       fileName];
   }
 
+  // For a diagnostic and the corresponding file Uri, we adjust the range of the
+  // diagnostic to skip over whitespace characters at the start of the line. This
+  // is a purely aesthetic and probably somewhat slow operation, but highlighting
+  // potential many whitespace characters looks quite bad.
+  private async adjustRangeStart(uri: vscode.Uri, diags: Array<vscode.Diagnostic>): Promise<Array<vscode.Diagnostic>> {
+    const bytes = await vscode.workspace.fs.readFile(uri);
+    const lines = bytes.toString().split("\n");
+    diags.forEach((diag, _idx, _arr) => {
+      const line = lines[diag.range.start.line];
+      diag.range = new vscode.Range(diag.range.start.line, line.search(/\S|$/), diag.range.end.line, 999);
+    });
+    return diags;
+  }
+
+  // The main entry point for calling out to clang, providing a mapping from
+  // filenames to arrays of errors
   private linkProject(): Map<string, Array<vscode.Diagnostic>> {
     let diags: Map<string, Array<vscode.Diagnostic>> = new Map();
     const folders = vscode.workspace.workspaceFolders;
     if (!folders) {
       return diags;
     }
-    const options: child_process.ExecOptions = {
-      cwd: folders[0].uri.fsPath
+    const options: child_process.SpawnSyncOptions = {
+      cwd: folders[0].uri.fsPath,
+      encoding: 'utf8',
+      shell: true
     };
-    child_process.exec(
-      "/home/karl/pirate/pirate-llvm/build/bin/clang -fuse-ld=lld -fdiagnostics-print-source-range-info -fdiagnostics-parseable-fixits -ffunction-sections -fdata-sections -Wl,-enclave,low *.c",
-      options,
-      (error, stdout, stderr) => {
-        const lines = stderr.split("\n");
-        let currLine = 0;
-        while (currLine < lines.length) {
-          let parts = lines[currLine].split(":");
-          if (this.isLinkerConflict(parts[2])) {
-            const [diag, fileName] = this.parseLinkerError(parts[2], lines[++currLine]);
-            let currentDiags = diags.get(fileName);
-            if (currentDiags) {
-              currentDiags.push(diag);
-              diags.set(fileName, currentDiags);
-            } else {
-              diags.set(fileName, [diag]);
-            }
-          } else {
-            console.log("ignored line: " + lines[currLine]);
-          }
-          currLine++;
+    let procResult = child_process.spawnSync(
+      "/home/karl/galois/pirate/pirate-llvm/build/bin/clang",
+      ["-g",
+        "-fuse-ld=lld",
+        "-ffunction-sections",
+        "-fdata-sections",
+        "-Wl,-enclave,low",
+        "*.c"
+      ],
+      options);
+
+    const errText = procResult.stderr.toString('utf8');
+    const lines = errText.split("\n");
+    let currLine = 0;
+    while (currLine < lines.length - 1) {
+      if (this.isLinkerConflict(lines[currLine])) {
+        const parts = lines[currLine].split(":");
+        const [diag, fileName] = this.parseLinkerError(parts[2], lines[currLine + 1]);
+        currLine++;
+        const currentDiags = diags.get(fileName);
+        if (currentDiags) {
+          currentDiags.push(diag);
+          diags.set(fileName, currentDiags);
+        } else {
+          diags.set(fileName, [diag]);
         }
-      });
+      }
+      currLine++;
+    }
     return diags;
   }
 }
