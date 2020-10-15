d8bb4fe385df688ddea450d300dfc9653ac72d3b
Karl Smeltzer <karl.smeltzer@gmail.com>
2020-10-15 09:21:05 -0700

    some linker error parsing

--- vscode-plugins/linker-errors/out/linkerRunner.js
+++ vscode-plugins/linker-errors/out/linkerRunner.js
@@ -16,19 +16,40 @@ class LinkerRunner {
     runLinker(diagnosticCollection) {
         return __awaiter(this, void 0, void 0, function* () {
             let diagnostics = this.linkProject();
+            for (let file of diagnostics.keys()) {
+                const fileDiags = diagnostics.get(file);
+                if (fileDiags) {
+                    vscode.workspace.findFiles(file, null, 1).then((uriArray) => {
+                        diagnosticCollection.set(uriArray[0], fileDiags);
+                    });
+                }
+            }
         });
     }
-    parseOutputLine(line) {
-        let start = new vscode.Position(3, 1);
-        let end = new vscode.Position(3, 120);
+    // This is a bit of a hack, but currently the linker conflict lines are of the
+    // form "Symbol <name> needed, but has unmet requirement <capability>".  For
+    // now, it seems sufficient to just look at the first word.
+    isLinkerConflict(text) {
+        console.log("Checking: " + text);
+        return text.trimLeft().startsWith("Symbol");
+    }
+    parseLinkerError(err, loc) {
+        const errWords = err.split(" ");
+        const symbolName = errWords[1];
+        const capability = errWords[7];
+        const locParts = loc.split(":");
+        const fileName = locParts[0].split(" ")[2];
+        const lineNumber = parseInt(locParts[1]);
+        let start = new vscode.Position(lineNumber, 0);
+        let end = new vscode.Position(lineNumber, 999);
         let range = new vscode.Range(start, end);
         return [
-            new vscode.Diagnostic(range, "Karl's diagnostic", vscode.DiagnosticSeverity.Error),
-            "foo.c"
+            new vscode.Diagnostic(range, "Reference to symbol " + symbolName + " requires the function to have capability " + capability, vscode.DiagnosticSeverity.Error),
+            fileName
         ];
     }
     linkProject() {
-        let diags = new Array();
+        let diags = new Map();
         const folders = vscode.workspace.workspaceFolders;
         if (!folders) {
             return diags;
@@ -36,17 +57,27 @@ class LinkerRunner {
         const options = {
             cwd: folders[0].uri.fsPath
         };
-        child_process.exec("/home/karl/galois/pirate/pirate-llvm/build/bin/clang -fuse-ld=lld -fdiagnostics-print-source-range-info -fdiagnostics-parseable-fixits -ffunction-sections -fdata-sections -Wl,-enclave,green *.c", options, (error, stdout, stderr) => {
-            let lines = stderr.split("\n");
-            lines.forEach((line, idx, arr) => {
-                let parts = line.split(":");
-                if (parts[0] === "ld.lld") {
-                    diags.push(this.parseOutputLine(parts[2]));
+        child_process.exec("/home/karl/pirate/pirate-llvm/build/bin/clang -fuse-ld=lld -fdiagnostics-print-source-range-info -fdiagnostics-parseable-fixits -ffunction-sections -fdata-sections -Wl,-enclave,low *.c", options, (error, stdout, stderr) => {
+            const lines = stderr.split("\n");
+            let currLine = 0;
+            while (currLine < lines.length) {
+                let parts = lines[currLine].split(":");
+                if (this.isLinkerConflict(parts[2])) {
+                    const [diag, fileName] = this.parseLinkerError(parts[2], lines[++currLine]);
+                    let currentDiags = diags.get(fileName);
+                    if (currentDiags) {
+                        currentDiags.push(diag);
+                        diags.set(fileName, currentDiags);
+                    }
+                    else {
+                        diags.set(fileName, [diag]);
+                    }
                 }
                 else {
-                    console.log("other: " + line);
+                    console.log("ignored line: " + lines[currLine]);
                 }
-            });
+                currLine++;
+            }
         });
         return diags;
     }
--- vscode-plugins/linker-errors/src/linkerRunner.ts
+++ vscode-plugins/linker-errors/src/linkerRunner.ts
@@ -4,24 +4,46 @@ import * as child_process from "child_process";
 export class LinkerRunner {
   public async runLinker(diagnosticCollection: vscode.DiagnosticCollection) {
     let diagnostics = this.linkProject();
+    for (let file of diagnostics.keys()) {
+      const fileDiags = diagnostics.get(file);
+      if (fileDiags) {
+        vscode.workspace.findFiles(file, null, 1).then((uriArray) => {
+          diagnosticCollection.set(uriArray[0], fileDiags);
+        });
+      }
+    }
   }
 
-  private parseOutputLine(line: string): [vscode.Diagnostic, string] {
+  // This is a bit of a hack, but currently the linker conflict lines are of the
+  // form "Symbol <name> needed, but has unmet requirement <capability>".  For
+  // now, it seems sufficient to just look at the first word.
+  private isLinkerConflict(text: string): boolean {
+    console.log("Checking: " + text);
+    return text.trimLeft().startsWith("Symbol");
+  }
 
+  private parseLinkerError(err: string, loc: string): [vscode.Diagnostic, string] {
+    const errWords = err.split(" ");
+    const symbolName = errWords[1];
+    const capability = errWords[7];
 
-    let start: vscode.Position = new vscode.Position(3, 1);
-    let end: vscode.Position = new vscode.Position(3, 120);
+    const locParts = loc.split(":");
+    const fileName = locParts[0].split(" ")[2];
+    const lineNumber = parseInt(locParts[1]);
+
+    let start: vscode.Position = new vscode.Position(lineNumber, 0);
+    let end: vscode.Position = new vscode.Position(lineNumber, 999);
     let range: vscode.Range = new vscode.Range(start, end);
     return [
       new vscode.Diagnostic(
         range,
-        "Karl's diagnostic",
+        "Reference to symbol " + symbolName + " requires the function to have capability " + capability,
         vscode.DiagnosticSeverity.Error),
-      "foo.c"];
+      fileName];
   }
 
-  private linkProject(): Array<[vscode.Diagnostic, string]> {
-    let diags: Array<[vscode.Diagnostic, string]> = new Array();
+  private linkProject(): Map<string, Array<vscode.Diagnostic>> {
+    let diags: Map<string, Array<vscode.Diagnostic>> = new Map();
     const folders = vscode.workspace.workspaceFolders;
     if (!folders) {
       return diags;
@@ -30,18 +52,27 @@ export class LinkerRunner {
       cwd: folders[0].uri.fsPath
     };
     child_process.exec(
-      "/home/karl/galois/pirate/pirate-llvm/build/bin/clang -fuse-ld=lld -fdiagnostics-print-source-range-info -fdiagnostics-parseable-fixits -ffunction-sections -fdata-sections -Wl,-enclave,green *.c",
+      "/home/karl/pirate/pirate-llvm/build/bin/clang -fuse-ld=lld -fdiagnostics-print-source-range-info -fdiagnostics-parseable-fixits -ffunction-sections -fdata-sections -Wl,-enclave,low *.c",
       options,
       (error, stdout, stderr) => {
-        let lines = stderr.split("\n");
-        lines.forEach((line, idx, arr) => {
-          let parts = line.split(":");
-          if (parts[0] === "ld.lld") {
-            diags.push(this.parseOutputLine(parts[2]));
+        const lines = stderr.split("\n");
+        let currLine = 0;
+        while (currLine < lines.length) {
+          let parts = lines[currLine].split(":");
+          if (this.isLinkerConflict(parts[2])) {
+            const [diag, fileName] = this.parseLinkerError(parts[2], lines[++currLine]);
+            let currentDiags = diags.get(fileName);
+            if (currentDiags) {
+              currentDiags.push(diag);
+              diags.set(fileName, currentDiags);
+            } else {
+              diags.set(fileName, [diag]);
+            }
           } else {
-            console.log("other: " + line);
+            console.log("ignored line: " + lines[currLine]);
           }
-        });
+          currLine++;
+        }
       });
     return diags;
   }
