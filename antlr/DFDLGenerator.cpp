91fccb8ea2eb87efb648d0b09404b25ab5432d29
Michael Spiegel <michael.m.spiegel@gmail.com>
2021-01-18 19:23:22 -0500

    DFDL range support and Apache License generation (#178)
    * DFDL generator add Apache License to generated schema
    
    * Add DFDL minInclusive and maxInclusive data validation
    
    Co-authored-by: Michael Spiegel <michael.spiegel@twosixlabs.com>

--- antlr/DFDLGenerator.cpp
+++ antlr/DFDLGenerator.cpp
@@ -24,6 +24,23 @@ using namespace ticpp;
 
 namespace {
 
+const std::string license_header = "Licensed to the Apache Software Foundation (ASF) under one\n\
+or more contributor license agreements.  See the NOTICE file\n\
+distributed with this work for additional information\n\
+regarding copyright ownership.  The ASF licenses this file\n\
+to you under the Apache License, Version 2.0 (the\n\
+\"License\"); you may not use this file except in compliance\n\
+with the License.  You may obtain a copy of the License at\n\
+\n\
+  http://www.apache.org/licenses/LICENSE-2.0\n\
+\n\
+Unless required by applicable law or agreed to in writing,\n\
+software distributed under the License is distributed on an\n\
+\"AS IS\" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY\n\
+KIND, either express or implied.  See the License for the\n\
+specific language governing permissions and limitations\n\
+under the License.";
+
 [[noreturn]] void
 not_implemented(char const* err)
 {
@@ -355,6 +372,9 @@ void construct_member(Element* e, Declarator* decl, TypeSpec* type, bool packed)
     for (auto ann : decl->annotations) {
         if (auto * valueAnn = dynamic_cast<ValueAnnotation*>(ann)) {
             e->SetAttribute("fixed", valueAnn->value);
+        } else if (auto * rangeAnn = dynamic_cast<RangeAnnotation*>(ann)) {
+            e->SetAttribute("minInclusive", rangeAnn->min);
+            e->SetAttribute("maxInclusive", rangeAnn->max);
         }
     }
 
@@ -399,7 +419,9 @@ int generate_dfdl(
         element->SetAttribute("name", get_type_name(def));
         finish_type(element, def, moduleDecl->packed);
     }
-
+    ostream << "<!--" << std::endl;
+    ostream << license_header << std::endl;
+    ostream << "-->" << std::endl;
     ostream << doc;
     return 0;
 }
