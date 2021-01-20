fadcf8fbb799f8db90c493084f3a42e19cb7dac4
Michael Spiegel <michael.spiegel@twosixlabs.com>
2021-01-20 09:18:03 -0500

    DFDL code generation feedback

--- antlr/DFDLGenerator.cpp
+++ antlr/DFDLGenerator.cpp
@@ -24,22 +24,20 @@ using namespace ticpp;
 
 namespace {
 
-const std::string license_header = "Licensed to the Apache Software Foundation (ASF) under one\n\
-or more contributor license agreements.  See the NOTICE file\n\
-distributed with this work for additional information\n\
-regarding copyright ownership.  The ASF licenses this file\n\
-to you under the Apache License, Version 2.0 (the\n\
-\"License\"); you may not use this file except in compliance\n\
-with the License.  You may obtain a copy of the License at\n\
+const std::string license_header = "  Licensed to the Apache Software Foundation (ASF) under one or more\n\
+  contributor license agreements.  See the NOTICE file distributed with\n\
+  this work for additional information regarding copyright ownership.\n\
+  The ASF licenses this file to You under the Apache License, Version 2.0\n\
+  (the \"License\"); you may not use this file except in compliance with\n\
+  the License.  You may obtain a copy of the License at\n\
 \n\
-  http://www.apache.org/licenses/LICENSE-2.0\n\
+      http://www.apache.org/licenses/LICENSE-2.0\n\
 \n\
-Unless required by applicable law or agreed to in writing,\n\
-software distributed under the License is distributed on an\n\
-\"AS IS\" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY\n\
-KIND, either express or implied.  See the License for the\n\
-specific language governing permissions and limitations\n\
-under the License.";
+  Unless required by applicable law or agreed to in writing, software\n\
+  distributed under the License is distributed on an \"AS IS\" BASIS,\n\
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n\
+  See the License for the specific language governing permissions and\n\
+  limitations under the License.";
 
 [[noreturn]] void
 not_implemented(char const* err)
@@ -72,6 +70,7 @@ set_defaults(Element* defaults, bool packed)
     defaults->SetAttribute("encoding", "utf-8");
     defaults->SetAttribute("encodingErrorPolicy", "replace");
     defaults->SetAttribute("escapeSchemeRef", "");
+    defaults->SetAttribute("fillByte", "%#r20;");
     defaults->SetAttribute("floating", "no");
     defaults->SetAttribute("ignoreCase", "no");
     defaults->SetAttribute("initiatedContent", "no");
